c4a0571c9438db5604f2f798fc1246ad1f307af1
jonathanFavorel <favorel.jonathan@outlook.fr>
2024-04-03 11:06:48 +0200

    Add TailWind CSS framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">Mon Portfolio</h1>
+        <p className="text-gray-700">Bienvenue sur mon portfolio !</p>
+      </div>
     </div>
   );
 }
